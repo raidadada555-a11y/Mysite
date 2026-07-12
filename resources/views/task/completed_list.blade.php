2718341b06dd5aa7704d8cd9fcf89daf768a6515
raidadada555-a11y <raidadada555@gmail.com>
2026-07-12 20:24:07 +0900

    Create completed task list view in Blade template

--- resources/views/task/completed_list.blade.php
+++ resources/views/task/completed_list.blade.php
@@ -1,1 +1,27 @@
+@extends('layouts.app')
 
+@section('content')
+<div class="container">
+    <h1>完了タスク一覧</h1>
+    <table class="table">
+        <thead>
+            <tr>
+                <th>タスク名</th>
+                <th>期限</th>
+                <th>重要度</th>
+                <th>詳細</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach($completedTasks as $task)
+            <tr>
+                <td>{{ $task->title }}</td>
+                <td>{{ $task->due_date }}</td>
+                <td>{{ $task->priority }}</td>
+                <td>{{ $task->description }}</td>
+            </tr>
+            @endforeach
+        </tbody>
+    </table>
+</div>
+@endsection
